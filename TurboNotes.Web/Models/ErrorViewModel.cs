0fecb5ff6e544ba5a38f72833b639aff20436fed
Oleksandr Karpinskyi <redfield242@gmail.com>
2025-05-10 13:47:55 +0300

    Initialize project, installed EF Core & SignalR packages

---
+++ TurboNotes.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TurboNotes.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
